368e4f7d111a81c730e622fba8c432b6f28913f2
shin04 <daikon0413@gmail.com>
2019-10-18 00:07:35 +0900

    Initial Commit

---
+++ rejack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rejack
+//
+//  Created by 梶原大進 on 2019/10/18.
+//  Copyright © 2019年 梶原大進. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
