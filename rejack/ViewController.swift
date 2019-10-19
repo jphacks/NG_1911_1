512b87da5cf70e397db2793686d13126db3a393e
shin04 <daikon0413@gmail.com>
2019-10-19 10:12:27 +0900

    first commit

--- rejack/ViewController.swift
+++ rejack/ViewController.swift
@@ -7,14 +7,46 @@
 //
 
 import UIKit
+import CoreNFC
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, NFCNDEFReaderSessionDelegate {
+    var session: NFCNDEFReaderSession?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) {
+        print(error.localizedDescription)
+    }
+    
+    func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) {
+        for message in messages {
+            for record in message.records {
+                if let string = String(data: record.payload, encoding: .ascii) {
+                    print(string)
+                }
+            }
+        }
+    }
+    
+    @IBAction func readNFC(_ sender: UIButton) {
+        print("button pushed!!")
+        guard NFCNDEFReaderSession.readingAvailable else {
+            let alertController = UIAlertController(
+                title: "Scanning Not Supported",
+                message: "This device doesn't support tag scanning.",
+                preferredStyle: .alert
+            )
+            alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alertController, animated: true, completion: nil)
+            return
+        }
+        
+        session = NFCNDEFReaderSession(delegate: self, queue: nil, invalidateAfterFirstRead: false)
+        session?.alertMessage = "Hold your iPhone near the item to learn more about it."
+        session?.begin()
+    }
+    
 }
 
